f143d30181fedfd443d16ad8fbde9089df84d0c8
Harry Coultas Blum <l@baz.codes>
2021-12-04 16:52:45 +0000

    Late day c1

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -1,8 +1,17 @@
 use std::fs::File;
-use std::path::Path;
 use std::io::{BufRead, BufReader};
-
+use std::path::Path;
 fn main() {
+    // println!("Day 1");
+    // println!("Challenge 1");
+    // ch1();
+    // println!("Challenge 2");
+    ch2();
+}
+
+// Use this to stop the warnings about the function not being used!
+#[allow(dead_code)]
+fn ch1() {
     // Create a path to the desired file
     let path = Path::new("/home/h/input.txt");
     let file = File::open(path).unwrap();
@@ -11,7 +20,14 @@ fn main() {
     // Read the file line by line using the lines() iterator from std::io::BufRead.
     let mut lines = reader.lines().enumerate();
     // firstline = firstline.
-    let mut prev: i32 = lines.next().unwrap().1.unwrap().trim().parse::<i32>().unwrap();
+    let mut prev: i32 = lines
+        .next()
+        .unwrap()
+        .1
+        .unwrap()
+        .trim()
+        .parse::<i32>()
+        .unwrap();
     let mut increased = 0;
 
     for (_index, line) in lines {
@@ -25,5 +41,59 @@ fn main() {
         }
         prev = lineint;
     }
+    println!("{}", increased)
+}
+
+fn parse_float(line: &String) -> f32 {
+    line.trim().parse::<f32>().expect("Failed to parse float")
+}
+
+#[allow(dead_code)]
+fn ch2() {
+    // Create a path to the desired file
+    let path = Path::new("./day1_input.txt");
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+
+    // Read the file line by line using the lines() iterator from std::io::BufRead.
+    let lines: Vec<String> = reader
+        .lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect();
+
+    println!("{}", lines[0]);
+    let mut increased = 0;
+    for i in 0..lines.len() - 3 {
+        let mut mov1: f32 = 0.;
+        for j in 0..3 {
+            // dbg!(j);
+            mov1 += parse_float(&lines[i + j]);
+        }
+        println!(
+            "mov1=[{},{},{}]={}",
+            lines[i + 0],
+            lines[i + 1],
+            lines[i + 2],
+            mov1
+        );
+
+        let mut mov2: f32 = 0.;
+        for j in 1..4 {
+            // dbg!(j);
+            mov2 += parse_float(&lines[i + j]);
+        }
+        println!(
+            "mov2=[{},{},{}]={}",
+            lines[i + 1],
+            lines[i + 2],
+            lines[i + 3]
+            mov2
+        );
+        if mov2 > mov1 {
+            increased += 1;
+        }
+        println!("{}", mov2 > mov1);
+    }
+
     println!("{}", increased)
 }
